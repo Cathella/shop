443f1cc3da0c30855f04a65c1f2b51e34d95c5d4
Cathella <ncathie214@gmail.com>
2021-04-04 17:40:22 +0300

    add conversations controller

---
+++ app/controllers/conversations_controller.rb
@@ -0,0 +1,27 @@
+class ConversationsController < ApplicationController
+  def create
+    @conversation = current_account.hato_conversations.build(conversation_params)
+    if @conversation.save
+      redirect_to conversation_path(@conversation)
+    else
+      redirect_to :conversations, notice: @conversation.errors
+    end
+  end
+
+  def show
+    @conversation = Denshobato::Conversation.find(params[:id])
+    @message_form = current_account.hato_messages.build
+    @messages = @conversation.messages.includes(:author)
+  end
+
+  def destroy
+    @conversation = Deshobato::Conversation.find(params[:id])
+    redirect_to :conversations if @conversation.destroy
+  end
+
+  private
+
+  def conversation_params
+    params.require(:deshobato_conversation).permit(:sender_id, :sender_type, :recipient_id, :recipient_type)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :resellers
 
-  resources :resellers, :customers, :conversations
+  resources :resellers, :customers, :conversations, :messages
 end
